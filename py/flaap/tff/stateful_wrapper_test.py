459233494af352e3ae3a88b800e34edac030ce45
Jeremy Lewi <jeremy@lewi.us>
2022-10-04 22:51:50 +0000

    Struct is working which causes e2e to work.

--- py/flaap/tff/stateful_wrapper_test.py
+++ py/flaap/tff/stateful_wrapper_test.py
@@ -12,6 +12,7 @@ from tensorflow_federated.python.core.impl.tensorflow_context import (
     tensorflow_computation,
 )
 from tensorflow_federated.python.core.impl.types import computation_types
+from tensorflow_federated.proto.v0 import executor_pb2
 
 # N.B looks like value_serialization gets moved to executor_serialization in 0.34
 if tensorflow_federated.__version__ < "0.34.0":
@@ -21,6 +22,8 @@ if tensorflow_federated.__version__ < "0.34.0":
 else:
     pass
 
+# N.B. The unittest for task_handler provides some additional coverage because it also
+# invokes the stateful_wrapper.
 
 def test_call():
     # Run a simple test. Verify that we can properly embed a simple
@@ -68,3 +71,31 @@ def test_call_with_arg():
     asyncio.run(wrapper.create_call("result", "somefunc", "somearg"))
     result = asyncio.run(wrapper.get_value("result").compute())
     assert result == 1010
+
+def test_create_struct_named():
+    # Test create struct with named fields
+    wrapper = stateful_wrapper.StatefulWrapper(
+        target_executor=eager_tf_executor.EagerTFExecutor()
+    )
+
+    asyncio.run(wrapper.create_value("someval", 10, tf.int32))
+
+    value_ref = executor_pb2.ValueRef(id="someval")
+    elements = [executor_pb2.CreateStructRequest.Element(name="a", value_ref=value_ref)]
+    asyncio.run(wrapper.create_struct("result",  elements))
+    result = asyncio.run(wrapper.get_value("result").compute())
+    assert result["a"] == 10
+
+def test_create_struct():
+    # Test create struct with unnamed fields
+    wrapper = stateful_wrapper.StatefulWrapper(
+        target_executor=eager_tf_executor.EagerTFExecutor()
+    )
+
+    asyncio.run(wrapper.create_value("someval", 10, tf.int32))
+
+    value_ref = executor_pb2.ValueRef(id="someval")
+    elements = [executor_pb2.CreateStructRequest.Element(value_ref=value_ref)]
+    asyncio.run(wrapper.create_struct("result",  elements))
+    result = asyncio.run(wrapper.get_value("result").compute())
+    assert result[0] == 10
